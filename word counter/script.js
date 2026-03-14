e11888b249202a5a71595df01f6427f747f9a93b
Ferdous Ali <ferdousali53@gmail.com>
2026-03-14 06:59:45 +0500

    Add files via upload

---
+++ word counter/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const input = document.querySelector("textarea");
+  const characterCount = document.querySelector("#characterCount");
+  const wordsCount = document.querySelector("#wordCount");
+  const sentenceCount = document.querySelector("#sentenceCount");
+
+  input.addEventListener("input", function () {
+    const text = input.value.trim();
+
+    //character count
+    characterCount.textContent = text.length;
+
+    //words count
+    const words = text.split(/\s+/).filter((word) => word.length);
+    wordsCount.textContent = words.length;
+
+    //sentence count
+
+    const sentences = text
+      .split(/[.!?]+/)
+      .filter((sentence) => sentence.length > 0);
+    sentenceCount.textContent = sentences.length;
+  });
+});
